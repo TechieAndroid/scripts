a9c863f1e59e7fae601bfbc6961246e6380f9ddf
Austin Kilgore <kilgorephotoshop@gmail.com>
2022-09-06 15:41:00 -0400

    first commit

---
+++ backup-vflared.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#DATE-TIME=date +%F_%R
+
+OF=/home/recompiler/backups/vflared/backup-$(date +%Y%m%d_%F_%R)
+DOCKER=/home/recompiler/docker
+HTTPD=/home/recompiler/docker/httpd
+
+echo "ENTERING DOCKER DIR"
+cd $DOCKER
+
+echo "SHUTTTING DOWN DOCKER"
+docker-compose down
+
+echo "MAKING BACKUP DIR"
+mkdir -p $OF
+
+echo "BACKING UP"
+cp -R $HTTPD $OF/
+
+echo "STARING DOCKER"
+docker-compose up -d
+
+echo "RETURNING HOME"
+cd
+
+echo "SETTING OWNERSHIP FOR BACKUP DIR"
+chown recompiler:recompiler -R $OF
+
+echo "DONE!"
